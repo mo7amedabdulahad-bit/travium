1dcb25b47ecf8c187d3c11c1a5be286a8424ae53
Mohamed Abdulahad <mo7amedabdulahad@gmail.com>
2025-12-27 19:59:30 +0400

    Fix #16: Remove 'used' column from admin VerificationListCtrl queries

--- src/admin/include/Controllers/VerificationListCtrl.php
+++ src/admin/include/Controllers/VerificationListCtrl.php
@@ -12,9 +12,9 @@ class VerificationListCtrl
         if (!isServerFinished() && isset($_REQUEST['section']) && $_REQUEST['section'] == 'del' && isset($_REQUEST['id'])) {
             $db->query("DELETE FROM activation WHERE id=" . (int)$_REQUEST['id']);
         }
-        $result = $db->query("SELECT * FROM activation WHERE used=0 AND worldId=" . getWorldUniqueId() . " ORDER BY id");
+        $result = $db->query("SELECT * FROM activation WHERE worldId=" . getWorldUniqueId() . " ORDER BY id");
         $params['content'] = null;
-        $params['total'] = $db->fetchScalar("SELECT COUNT(id) FROM activation WHERE used=0");
+        $params['total'] = $db->fetchScalar("SELECT COUNT(id) FROM activation");
         while ($row = $result->fetch_assoc()) {
             $params['content'] .= '<tr>';
             $params['content'] .= '<td style="text-align: center;"><a href="admin.php?action=verificationList&id=' . $row['id'] . '&section=del"><img src="img/x.gif" class="del"></a></td>';
