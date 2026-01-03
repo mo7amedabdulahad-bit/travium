fc84b46cbdd9ff0392bbb88e149d89c7290106d0
Mohamed Abdulahad <mo7amedabdulahad@gmail.com>
2026-01-03 11:29:41 +0400

    Fix: Added missing php tag to dummy_global_config.php

--- scripts/dummy_global_config.php
+++ scripts/dummy_global_config.php
@@ -1,3 +1,4 @@
+<?php
 // Dummy global config file to satisfy require_once in src/config.php
 // This is used by verification scripts running in CLI mode.
 global $globalConfig;
@@ -6,4 +7,3 @@ if (!isset($globalConfig)) $globalConfig = [];
 if (!defined("CONNECTION_FILE")) {
     define("CONNECTION_FILE", __DIR__ . '/dummy_connection.php');
 }
-
