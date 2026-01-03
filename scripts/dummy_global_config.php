f848f4cd00070a83e1caa099a267c332a30880e3
Mohamed Abdulahad <mo7amedabdulahad@gmail.com>
2026-01-03 11:31:06 +0400

    Fix: Initialized arrays in dummy_global_config.php to prevent undefined key errors

--- scripts/dummy_global_config.php
+++ scripts/dummy_global_config.php
@@ -4,6 +4,24 @@
 global $globalConfig;
 if (!isset($globalConfig)) $globalConfig = [];
 
+// Initialize required keys to prevent "Undefined array key" warnings in src/config.php
+if (!isset($globalConfig['staticParameters'])) $globalConfig['staticParameters'] = [];
+if (!isset($globalConfig['dataSources'])) $globalConfig['dataSources'] = [];
+if (!isset($globalConfig['voting'])) $globalConfig['voting'] = [];
+if (!isset($globalConfig['mailer'])) $globalConfig['mailer'] = [];
+
+// Mock essentials
+$globalConfig['staticParameters']['recaptcha_public_key'] = 'mock_key';
+$globalConfig['staticParameters']['recaptcha_private_key'] = 'mock_key';
+$globalConfig['staticParameters']['indexUrl'] = 'http://localhost/';
+$globalConfig['dataSources']['globalDB'] = [
+    'hostname' => 'localhost',
+    'username' => 'travium',
+    'password' => 'travium',
+    'database' => 'travium',
+    'charset' => 'utf8mb4'
+];
+
 if (!defined("CONNECTION_FILE")) {
     define("CONNECTION_FILE", __DIR__ . '/dummy_connection.php');
 }
