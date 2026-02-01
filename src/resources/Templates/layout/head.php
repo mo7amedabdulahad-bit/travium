fdfd7aa38ab99305a7ee055f99c8949b6e7cae3a
Mohamed Abdulahad <mo7amedabdulahad@gmail.com>
2026-02-01 14:06:40 +0400

    fix: Hide sidebars and replace statistics with mobile menu
    MOBILE CHANGES:
    1. Hide sidebars completely (display: none)
       - #sidebarBeforeContent hidden
       - #sidebarAfterContent hidden
    
    2. Statistics button replaced by mobile menu button
       - .statistics: display none
       - .mobileMenuButton: shown in navigation (not fixed corner)
       - Position: relative (flows with other nav buttons)
    
    Navigation buttons (7 total):
    - dorf1, dorf2, map
    - reports, messages, dailyQuests
    - mobileMenuButton (replaces statistics)
    - mobileShopButton
    
    This matches official Travian mobile behavior where:
    - Statistics accessed via mobile menu (not nav button)
    - Sidebars hidden on mobile (full screen content)

--- src/resources/Templates/layout/head.php
+++ src/resources/Templates/layout/head.php
@@ -21,9 +21,9 @@ use Core\Helper\PreferencesHelper;
 
     <?php endif; ?>
     <link href="<?=get_gpack_link_and_hash("fixes.css", false); ?>?rev13" rel="stylesheet" type="text/css"/>
-    <!-- Mobile Responsive Design - Fixed Structure -->
-    <link href="<?=get_gpack_cdn_base_url();?>responsive/mobile.css?v=fixed-structure" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="<?=get_gpack_cdn_base_url();?>responsive/mobile-touch.js?v=fixed-structure"></script>
+    <!-- Mobile Responsive Design - Hide Sidebars -->
+    <link href="<?=get_gpack_cdn_base_url();?>responsive/mobile.css?v=hide-sidebars" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="<?=get_gpack_cdn_base_url();?>responsive/mobile-touch.js?v=hide-sidebars"></script>
 
     <script type="text/javascript">
         window.ajaxToken = '<?=(isset($vars['ajaxToken']) ? $vars['ajaxToken'] : null);?>';
