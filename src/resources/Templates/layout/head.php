8db12fa9600006d0e7600bee7c7d52de1a226fa0
Mohamed Abdulahad <mo7amedabdulahad@gmail.com>
2026-02-01 12:38:38 +0400

    fix: Add scale(1.5) transform to StockBar on mobile
    Based on official Travian header-footer analysis (header-footer-1769934821109.json).
    
    Official Travian uses:
    - position: fixed
    - top: 0px
    - transform: scale(1.5)
    - transform-origin: top left
    - z-index: 38
    - width: 362px (not 100%)
    - right: 258px
    
    Changes:
    - Added transform: scale(1.5) to #stockBar
    - Added transform-origin: top left
    - Changed width from 100% to 362px
    - Changed right from 0 to 258px
    - Changed height from auto to 35.3333px
    
    This matches official behavior while keeping our superior
    flexbox navigation (official uses buggy transform causing
    only 6/8 buttons visible).

--- src/resources/Templates/layout/head.php
+++ src/resources/Templates/layout/head.php
@@ -21,9 +21,9 @@ use Core\Helper\PreferencesHelper;
 
     <?php endif; ?>
     <link href="<?=get_gpack_link_and_hash("fixes.css", false); ?>?rev13" rel="stylesheet" type="text/css"/>
-    <!-- Mobile Responsive Design - Visual Recording Analysis -->
-    <link href="<?=get_gpack_cdn_base_url();?>responsive/mobile.css?v=visual-fix" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="<?=get_gpack_cdn_base_url();?>responsive/mobile-touch.js?v=visual-fix"></script>
+    <!-- Mobile Responsive Design - StockBar Scale Fix -->
+    <link href="<?=get_gpack_cdn_base_url();?>responsive/mobile.css?v=stockbar-scale" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="<?=get_gpack_cdn_base_url();?>responsive/mobile-touch.js?v=stockbar-scale"></script>
 
     <script type="text/javascript">
         window.ajaxToken = '<?=(isset($vars['ajaxToken']) ? $vars['ajaxToken'] : null);?>';
