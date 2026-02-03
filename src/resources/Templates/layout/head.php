723c4319e50f1a832b373d978eaac2c3843752a3
Mohamed Abdulahad <mo7amedabdulahad@gmail.com>
2026-02-03 21:52:03 +0400

    chore: Update cache buster to fix2

--- src/resources/Templates/layout/head.php
+++ src/resources/Templates/layout/head.php
@@ -22,8 +22,8 @@ use Core\Helper\PreferencesHelper;
     <?php endif; ?>
     <link href="<?=get_gpack_link_and_hash("fixes.css", false); ?>?rev13" rel="stylesheet" type="text/css"/>
     <!-- Mobile Responsive Design - OFFICIAL TRAVIAN UI REPLICATION -->
-    <link href="<?=get_gpack_cdn_base_url();?>responsive/mobile.css?v=official-ui-fix1" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="<?=get_gpack_cdn_base_url();?>responsive/mobile-nav.js?v=official-ui-fix1"></script>
+    <link href="<?=get_gpack_cdn_base_url();?>responsive/mobile.css?v=official-ui-fix2" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="<?=get_gpack_cdn_base_url();?>responsive/mobile-nav.js?v=official-ui-fix2"></script>
 
     <script type="text/javascript">
         window.ajaxToken = '<?=(isset($vars['ajaxToken']) ? $vars['ajaxToken'] : null);?>';
