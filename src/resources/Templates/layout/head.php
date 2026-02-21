0cf0f93823c37b01e0a2aed7d53bf0b5ed35e850
Mohamed Abdulahad <mo7amedabdulahad@gmail.com>
2026-02-22 03:23:03 +0400

    head.php: add TravianDefaults inline block before crypt.js to fix window.Travian initialization

--- src/resources/Templates/layout/head.php
+++ src/resources/Templates/layout/head.php
@@ -40,6 +40,25 @@ use Core\Helper\PreferencesHelper;
     <script type="text/javascript" src="<?=get_gpack_cdn_mainPage_url();?>js/gsap/TweenMax.min.js"></script>
     <script type="text/javascript" src="<?=get_gpack_cdn_mainPage_url();?>js/gsap/plugins/MorphSVGPlugin.min.js"></script>
     <script type="text/javascript" src="<?=get_gpack_cdn_mainPage_url();?>js/PixiJS/pixi.min.js"></script>
+    <!-- TravianDefaults must be set BEFORE crypt.js which references it -->
+    <script type="text/javascript">
+        <?php
+        $data = [
+            'Map' => [
+                'Size' => [
+                    'width'  => (2 * MAP_SIZE) + 1,
+                    'height' => (2 * MAP_SIZE) + 1,
+                    'left'   => -MAP_SIZE,
+                    'right'  => MAP_SIZE,
+                    'bottom' => -MAP_SIZE,
+                    'top'    => MAP_SIZE,
+                ],
+            ],
+            'Season' => detect_season(),
+        ];
+        ?>
+        window.TravianDefaults = Object.assign(<?=json_encode($data);?>, false || {});
+    </script>
     <!-- crypt.js (T4.4): defines window.Travian namespace needed by inline setup scripts -->
     <script type="text/javascript" src="/integrations/cdn/3f73c13f/mainPage/js/crypt.js"></script>
     <script type="text/javascript" src="js/Game/General/General.js"></script>
